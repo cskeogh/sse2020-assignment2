42eb226ed491979120a1ca225651f47dacccc3cd
Craig S. Keogh <craig.s.keogh@gmail.com>
2020-09-16 22:38:07 +0930

    Add more answers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import git
 import os
 import os.path
 import re
+import util
 
 remote_link = "https://github.com/apache/cxf.git"
 local_link = "repo/cxf"
@@ -9,7 +10,7 @@ fixing_commit = "9deb2d179758d3da47ce3ea492c2606c0a6a8475"
 affected_files = \
     ["rt/rs/extensions/providers/src/main/java/org/apache/cxf/jaxrs/provider/atom/AbstractAtomProvider.java",
      "rt/rs/extensions/providers/src/test/java/org/apache/cxf/jaxrs/provider/atom/AtomPojoProviderTest.java"]
-
+frequently_identified_commit = {}
 
 class Progress(git.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
@@ -22,53 +23,113 @@ def assignment2():
     else:
         git.Repo.clone_from(remote_link, local_link, progress=Progress())
     repo = git.Repo(local_link)
+    assignment2_part3a(repo)
     assignment2_part3b(repo)
-
+    vcc = assignment2_part3c()
+    assignment2_part5a(repo, vcc)
+    assignment2_part5b(repo, vcc)
+    assignment2_part5c(repo, vcc)
+    totals = assignment2_part5de(repo, vcc)
+    assignment2_part5fg(repo, vcc, totals)
 
 def assignment2_part3a(repo):
-    print("Part 3.a -- lines deleted, last commit that modified --")
+    print("\nPart 3.a -- lines deleted, last commit that modified --")
     blame_data = repo.git.blame("-L92,+1", fixing_commit + "^", "--", affected_files[0])
     for line in blame_data.splitlines():
         print(line)
     blame_data = repo.git.blame("-L95,+1", fixing_commit + "^", "--", affected_files[0])
     for line in blame_data.splitlines():
+        commit_id = line.split(' ')[0]
+        if frequently_identified_commit.get(commit_id) is None:
+            frequently_identified_commit[commit_id] = 1
+        else:
+            frequently_identified_commit[commit_id] += 1
         print(line)
 
 
 def assignment2_part3b(repo):
-    print("Part 3.b -- lines added, smallest eclosing scope --")
+    print("\nPart 3.b -- lines added, smallest enclosing scope ---------------------")
     added_line = 92
-    enclosing_scope_lines = [0, 0]
-    method_sig_re = re.compile(r"(public|protected|private|static|\s) +[\w\<\>\[\]]+\s+(\w+) *\([^\)]*\)(?:\s*throws [\w.]+)?\s*\{")
-    bracket_re = re.compile(r"\{|\}")
+    raw_file_contents = repo.git.show(fixing_commit + "^" + ":" + affected_files[0])
+    enclosing_scope_lines = util.smallest_enclosing_scope(raw_file_contents, added_line)
 
-    raw_data = repo.git.show(fixing_commit + ":" + affected_files[0])
-    matches = method_sig_re.finditer('\n'.join(raw_data.splitlines()[:added_line]))
-    closest_method_sig = None
-    for closest_method_sig in matches:
-        pass
-    enclosing_scope_lines[0] = raw_data.count('\n', 0, closest_method_sig.start()) + 1
-
-    matches = bracket_re.finditer(raw_data, closest_method_sig.end())
-    nest_levels = 0
-    end_method_sig = None
-    for end_method_sig in matches:
-        if end_method_sig.group(0) == '{':
-            nest_levels += 1
-        elif end_method_sig.group(0) == '}':
-            if nest_levels == 0:
-                break
-            else:
-                nest_levels -= 1
-    if end_method_sig is None:
-        raise ValueError("Parse error")
-
-    enclosing_scope_lines[1] = raw_data.count('\n', closest_method_sig.start(), end_method_sig.end()) + 1
     blame_data = repo.git.blame("-L" + str(enclosing_scope_lines[0]) + ",+" + str(enclosing_scope_lines[1]),
-                                fixing_commit, "--", affected_files[0])
+                                fixing_commit + "^", "--", affected_files[0])
+
     for line in blame_data.splitlines():
+        commit_id = line.split(' ')[0]
+        if frequently_identified_commit.get(commit_id) is None:
+            frequently_identified_commit[commit_id] = 1
+        else:
+            frequently_identified_commit[commit_id] += 1
         print(line)
 
+
+def assignment2_part3c():
+    print("\nPart 3.c -- frequently identified commit as the VCC ---------------------")
+    vcc = max(frequently_identified_commit,key=frequently_identified_commit.get)
+    print(vcc)
+    return vcc
+
+
+def assignment2_part5a(repo, vcc):
+    print("\nPart 5.a -- message and title of VCC ---------------------")
+    git_output = repo.git.show(vcc, "-s")
+    print(git_output)
+
+
+def assignment2_part5b(repo, vcc):
+    print("\nPart 5.b -- total files affected by VCC ---------------------")
+    git_output = repo.git.show(vcc, "--stat=9999")
+    print(git_output)
+    match = next(re.finditer(r'(?P<filechanged>\d+) files changed', git_output))
+    if match is not None:
+        print(match.group('filechanged') + " files changed")
+
+
+def assignment2_part5c(repo, vcc):
+    print("\nPart 5.c -- total directories affected by VCC ---------------------")
+    git_output = repo.git.show(vcc, '--stat=9999', '--format=format:')
+    git_output = '\n'.join(git_output.splitlines()[:-1]) # drop the last line because it's a summary line
+    print(git_output)
+    dirs = {}
+    for filename_match in re.finditer(r'^\s*(?P<filename>[\w\\\/\.]+)', git_output, re.MULTILINE):
+        dirname = os.path.dirname(filename_match.group('filename'))
+        if dirs.get(dirname) is None:
+            dirs[dirname] = 1
+        else:
+            dirs[dirname] += 1
+    print(str(len(dirs.keys())) + " directories affected.")
+
+
+def assignment2_part5de(repo, vcc):
+    print("\nPart 5.d -- total deleted lines of code (including comments and blank lines) ---------------------")
+    print("Part 5.e -- total added lines of code (including comments and blank lines) -------------------------")
+    git_output = repo.git.show(vcc, '--numstat', '--format=format:')
+    print(git_output)
+    totals = {'del': 0, 'add': 0}
+    for match in re.finditer(r'^\s*(?P<add>\d+)\s+(?P<del>\d+)\s', git_output, re.MULTILINE):
+        totals['del'] += int(match.group('del'))
+        totals['add'] += int(match.group('add'))
+    print(str(totals['del']) + " total deleted lines of code (including comments and blank lines)")
+    print(str(totals['add']) + " total added lines of code (including comments and blank lines)")
+    return totals
+
+def assignment2_part5fg(repo, vcc, totals):
+    print("\nPart 5.f -- total deleted lines of code (excluding comments and blank lines) ---------------------")
+    print("Part 5.g -- total added lines of code (excluding comments and blank lines) -------------------------")
+    git_output = repo.git.show(vcc, '--format=format:')
+    print(git_output)
+    for match in re.finditer(r'^\-\s*$', git_output, re.MULTILINE):  # deleted blank lines
+        totals['del'] -= 1
+    for match in re.finditer(r'^\-\s*\\\\', git_output, re.MULTILINE):  # deleted comment lines
+        totals['del'] -= 1
+    for match in re.finditer(r'^\+\s*$', git_output, re.MULTILINE):  # added blank lines
+        totals['add'] -= 1
+    for match in re.finditer(r'^\+\s*\\\\', git_output, re.MULTILINE):  # added comment lines
+        totals['add'] -= 1
+    print(str(totals['del']) + " total deleted lines of code (excluding comments and blank lines)")
+    print(str(totals['add']) + " total added lines of code (excluding comments and blank lines)")
 
 if __name__ == '__main__':
     print('Assignment 2')
